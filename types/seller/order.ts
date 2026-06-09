c2782e1e73e3a1f6dbff4a51e5d44b339c15f77b
nana9147 <yuna.chu9147@gmail.com>
2026-06-09 17:51:40 +0900

    feat: 주문 관리 페이지 추가 (현황 카드) - WIP

--- types/seller/order.ts
+++ types/seller/order.ts
@@ -1,9 +1,53 @@
-export type OrderStatus = '배송준비중' | '결제완료' | '배송중' | '배송완료';
+export type OrderStatus = '결제완료' | '배송준비중' | '배송중' | '배송완료' | '취소' | '환불';
+export type PaymentMethod = '카드' | '실시간 계좌이체' | '간편결제' | '휴대폰 소액결제';
 
+export type OrderStatusFilter = OrderStatus | '전체' | '취소환불';
+
+//  주문 내역 목록
 export interface Order {
   id: string;
   customer: string;
   product: string;
-  price: string;
+  price: number;
   status: OrderStatus;
+  orderDate: string;
+}
+
+// 주문 상세 내역 - 개별 상품 정보
+export interface OrderProduct {
+  id: string;
+  itemName: string;
+  quantity: number;
+  itemTotalPrice: number;
+}
+
+// 주문 상세 내역 - 결제 정보
+export interface PaymentInfo {
+  totalPrice: number; // 전체 상품금액
+  shippingFee: number; // 배송비
+  couponType: string; // 쿠폰 종류
+  couponAmount: number; // 사용 쿠폰금액
+  pointAmount: number; // 사용 포인트
+  finalAmount: number; // 최종 결제 금액
+  paymentMethod: PaymentMethod; // 결제 수단
+}
+
+// 주문 상세 내역 - 배송 정보
+export interface DeliveryInfo {
+  name: string;
+  phone: string;
+  address: {
+    originAddress: string;
+    detailAddress: string;
+  };
+  memo: string;
+}
+
+export interface OrderDetail {
+  id: string;
+  orderDate: string;
+  status: OrderStatus;
+  products: OrderProduct[];
+  payment: PaymentInfo;
+  delivery: DeliveryInfo;
 }
