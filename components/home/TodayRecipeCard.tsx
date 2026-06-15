261e5763f05c4e35001db522f7c18490ff2fdadb
jbhong3010-ops <jbhong3010@gmail.com>
2026-06-15 22:53:53 +0900

    홈 화면 컴포넌트 분리 및 UI 구현

--- components/home/TodayRecipeCard.tsx
+++ components/home/TodayRecipeCard.tsx
@@ -9,7 +9,7 @@ export default function TodayRecipeCard() {
 
       <div className="h-[120px] bg-[#E7E1D3] rounded-xl mt-2 mb-3" />
 
-      <h4 className="font-medium">나홀로 불 비빔밥</h4>
+      <h4 className="font-medium">나홀로 열무 비빔밥</h4>
 
       <p className="text-xs text-gray-500">⭐ 4.8 (96)</p>
     </div>
