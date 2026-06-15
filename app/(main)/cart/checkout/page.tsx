00fc74f1631b1799ef0d0bffbfe8d2030106659e
djsy01 <dlsgh8884@naver.com>
2026-06-15 10:06:42 +0900

    refactor: components/order → components/cart 폴더 이동

--- app/(main)/cart/checkout/page.tsx
+++ app/(main)/cart/checkout/page.tsx
@@ -1,4 +1,4 @@
-import Checkout from '@/components/(auth)/order/checkout/Checkout';
+import Checkout from '@/components/(auth)/cart/checkout/Checkout';
 
 export default function CheckoutPage() {
   return <Checkout />;
