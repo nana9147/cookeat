981a866833360e3f4525837ade1785fd59af0e89
djsy01 <dlsgh8884@naver.com>
2026-06-22 17:54:39 +0900

    fix: next.config에 encrypted-tbn0.gstatic.com 이미지 도메인 추가

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'thumb.cjonstyle.net',
       },
     ],
